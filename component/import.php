8040fea74dc72965e0d99715525da396ecc28046
Jerome2028 <rivera.jerome.dc.1525@gmail.com>
2024-05-05 13:30:18 +0800

    static page

--- component/import.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require "controller/controller.session.php";
-require "controller/controller.utility.php";
-
-$s = new Session();
-$BASE = Utility::getBase();
-$utility= new Utility();
--- index.php
+++ index.php
@@ -1,10 +1,119 @@
-<?php
- 
-/**
- * Require the router Class
- * And render current view based on the URL request
- */
- require_once "controller/controller.router.php";
- 
- $router = new Router($_SERVER['REQUEST_URI']);
- $router->getView();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My portfolio | work.me</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Inmed Corporation">
+    <meta name="author" content="Jerome Rivera">
+    <meta name="viewport" 
+    content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <link href="assets/img/code.png" rel="icon">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/vendor/lib/animate/animate.css" rel="stylesheet">
+    <link href="assets/vendor/lib/boxicons/css/boxicons.css" rel="stylesheet">
+    <link href="assets/vendor/lib/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    <link href="assets/vendor/lib/div-aos/css/style.css" rel="stylesheet">
+
+    <script src ="assets/js/jquery.min.js"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js" integrity="sha512-+k1pnlgt4F1H8L7t3z95o3/KO+o78INEcXTbnoJQ/F2VqDVhWoaiVml/OEHv9HsVgxUaVW+IbiZPUJQfF/YxZw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+</head>
+<body>
+<div id="header" class="container-fluid header-transparent">
+  <div class="container d-flex justify-content-between align-items-center">
+    <div class="logo">
+      <a href=""><img src="assets/img/code.png">Work.me</a>
+    </div>
+
+    <div id="navbar" class="navbar">
+    <ul>
+        <li class="waves-effect waves-light"><a class="nav-link" href="">Home</a></li>
+        <li class="waves-effect waves-light"><a class="nav-link" href="about-us/">About Us</a></li>
+
+        <li class="waves-effect waves-light"><a class="nav-link" href="contact-us/">Contact Us</a></li>
+    </ul>
+      <i class="bi bi-list mobile-nav-toggle" data-bs-toggle="offcanvas" data-bs-target="#navbarr"></i>
+    </div>
+      <!-- .navbar -->
+      <div class="offcanvas offcanvas-end nav-bar" id="navbarr" tabindex="-1" aria-labelledby="offcanvasRightLabel">
+        <div class="offcanvas-header p-3">
+          <h3 class="offcanvas-title">Menu</h3>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
+        </div>
+        <hr>
+        <div class="offcanvas-body">
+          
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item waves-effect waves-light">
+            <a class="nav-link" href="">Home</a>
+          </li>
+    
+          <li class="nav-item dropdown ">
+            <a class="nav-link dropdown-toggle fw-normal" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              About us
+            </a>
+            <ul class="dropdown-menu shadow position-static">
+              <li><a class="nav-link  dropdown-item" href="about-us">About Us</a></li>
+              <li><a class="nav-link> dropdown-item" href="awards">Awards</a></li>
+              <li><a class="nav-link  dropdown-item" href="careers">Careers</a></li>
+            </ul>
+          </li>
+            <li class="nav-item waves-effect waves-light"><a class="nav-link " href="news-events">News and Events</a></li>
+            <li class="nav-item waves-effect waves-light"><a class="nav-link " href="products">Products</a></li>
+            <li class="nav-item waves-effect waves-light"><a class="nav-link " href="brochures">Brochures</a></li>
+            <li class="nav-item waves-effect waves-light "><a class="nav-link " href="contact-us">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="loader-wrapper">
+    <span class="loader"><span class="loader-inner"></span></span>
+</div>
+<main>
+ <section id="banner">
+    <div class="container ">
+        <div class="row">
+            <div class="col-8">
+            <h1 class="animate__animated animate__fadeIn animate__slow "><i class="bx bxs-quote-alt-left quote-icon-left"></i>&nbsp;The only way to do <span class="portfolio-color">great work</span> <br>is to  <span class="portfolio-color">love </span> you  <span class="portfolio-color">do.</span><i class="bx bxs-quote-alt-right quote-icon-right"></i></h1>
+            <p>Hi there! I'm a skilled LAMP stack developer. I specialize in crafting dynamic web solutions with efficiency and precision. Let's build something great together!</p>
+            </div>
+        </div>
+    </div>
+ </section>
+ <section>
+    <div class="container">
+        <p>testt estsetse </p>
+
+    </div>
+ </section>
+</main>
+<footer id="footer">
+
+</div>
+<script>
+$(window).on("load",function(){
+    $(".loader-wrapper").fadeOut("slow");
+});
+</script>
+</footer>
+<div class="bg-dark meta-footer p-1"><div class="container"><p class="text-white text-center mb-0">© 2024. INMED CORPORATION ALL RIGHTS RESERVED.</p></div></div>
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center text-white"><box-icon name='chevron-up' type='solid' animation='tada' color='white 'rotate='90' ></box-icon></a>
+<!-- Template Main JS File -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src ="assets/vendor/lib/boxicons/js/script.js"></script>
+<script src ="assets/vendor/lib/bootstrap/js/bootstrap.bundle.js"></script>
+<script src ="assets/vendor/lib/div-aos/js/script.js"></script>
+  <script src ="assets/js/main.js"></script>
+  <!-- <script src ="assets/js/contact.js"></script> -->
+  </body>
+  </html>
+
+
+
