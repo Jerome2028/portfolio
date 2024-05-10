bda61146ab75213f4f7d521656cd9e898e9f83e6
unknown <rivera.jerome.dc.1525@gmail.com>
2024-05-10 17:34:23 +0800

    email

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,21 @@
     }
   }
 
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+
   /**
    * Easy on scroll event listener 
    */
